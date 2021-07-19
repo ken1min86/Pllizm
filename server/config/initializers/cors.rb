bf9e8953cca2220c5857f7ed414392943552eb78
Ken0806 <ken1min86@gmail.com>
2021-07-19 10:29:08 +0900

    [add]

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:4020/'
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true,
+             expose: %w[access-token uid client expiry]
   end
 end
