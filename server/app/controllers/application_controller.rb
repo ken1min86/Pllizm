911bad6655f8a64919761ab3dcb9e73633cdb022
Ken0806 <ken1min86@gmail.com>
2021-07-31 18:30:38 +0900

    [add]Deise token authの導入およびセットアップ
    Deise token authを利用して、サインアップ・サインイン・パスワード変更を可能にした。

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # APIではCSRFチェックをしないように設定
+  skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false
+
+  #   以下のように、サインアップ時に自動でusernameを設定する用にする
+  #   before_action :configre_permitted_parameters, if: :devise_controller?
+  # 参考 https://qiita.com/neojin/items/f03d8ebfc2e9e8a31daa
+  #   def configre_permitted_parameters
+  #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # デフォルトのnameを設定する処理
+  #   end
 end
