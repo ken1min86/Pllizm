c4bd1ff48e8f8b4a254f13a10a699363c00b480c
Ken0806 <ken1min86@gmail.com>
2021-08-04 17:41:39 +0900

    [add]UsersのusernameカラムにRailsのバリデーションを追加

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   include DeviseTokenAuth::Concerns::User
   validates :name, length: { maximum: 50 }, presence: true
+  validates :name, length: { minimum: 1, maximum: 50 }, presence: true
   validates :bio,  length: { minimum: 1, maximum: 160 }, allow_nil: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true
