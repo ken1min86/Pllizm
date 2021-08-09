1cac18224a5d91f85ec76f505a969681fb70427f
Ken0806 <ken1min86@gmail.com>
2021-08-09 17:23:58 +0900

    [update]UsersのimageにImageUploaderをマウントし、機能・テスト修正
    画像の拡張子のバリデーションを、CarrierWaveの機能を使用して実施するよう修正した。
    また、それに伴うテストの修正も実施した。

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   acts_as_paranoid
-
-  has_many :posts, dependent: :destroy
+  mount_uploader :image, ImageUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # :confirmable, :omniauthable
   devise :validatable, password_length: 8..128
   include DeviseTokenAuth::Concerns::User
+
+  has_many :posts, dependent: :destroy
 
   validates :userid,   length: { maximum: 15 }, uniqueness: true, presence: true
   validates :username, length: { maximum: 50 }, presence: true
