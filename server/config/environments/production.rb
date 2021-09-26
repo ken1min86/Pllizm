74b345ab51a54f554f1de922bcf287a0aaf81b68
Ken0806 <ken1min86@gmail.com>
2021-09-26 17:42:34 +0900

    [add]Deviseの日本語対応

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -60,7 +60,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "localhost", port: 3020 }
+  config.action_mailer.default_url_options = { host: ENV['CLIENT_PROD_HOST'] }
+  config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['EMAIL_ADDRESS'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
