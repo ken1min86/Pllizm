a08090824b43e1d6a8c2a071bf79eb1f76daa9a5
Ken0806 <ken1min86@gmail.com>
2021-10-24 13:37:44 +0900

    [add]リフラクト一覧ページのヘッダーを作成

--- client/src/components/pages/RefractCandidates.tsx
+++ client/src/components/pages/RefractCandidates.tsx
@@ -1,11 +1,33 @@
 import { DefaultTemplate } from 'components/templates';
 import { VFC } from 'react';
 
+import { Box, Theme } from '@mui/material';
+import createStyles from '@mui/styles/createStyles';
+import makeStyles from '@mui/styles/makeStyles';
+
+import { RefractFuncDescriptionModal } from '../organisms';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      color: theme.palette.primary.main,
+      fontSize: 22,
+    },
+  }),
+)
+
 const RefractCandidates: VFC = () => {
-  console.log(2)
+  const classes = useStyles()
+
+  const Header = (
+    <Box sx={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} pl={3} pr={2}>
+      <h1 className={classes.title}>refract</h1>
+      <RefractFuncDescriptionModal type="questionButton" />
+    </Box>
+  )
 
   return (
-    <DefaultTemplate activeNavTitle="refract" Header={undefined} Bottom={undefined}>
+    <DefaultTemplate activeNavTitle="refract" Header={Header} Bottom={undefined}>
       dd
     </DefaultTemplate>
   )
