0626a0b2b865cdf419067f9141656e54a966b750
Ken0806 <ken1min86@gmail.com>
2021-10-25 17:56:14 +0900

    [add]リフラクトスキップ確認モーダルの実装

--- client/src/components/organisms/modal/SkipRefractModal.tsx
+++ client/src/components/organisms/modal/SkipRefractModal.tsx
@@ -1,33 +1,41 @@
-import { OutlinedRoundedCornerButton } from 'components/atoms';
-import { useState } from 'react';
+import {
+    ContainedRoundedCornerButton, ErrorMessage, OutlinedRoundedCornerButton
+} from 'components/atoms';
+import { ModalForRefract } from 'components/molecules';
+import useSkipRefract from 'hooks/useSkipRefract';
+import { useState, VFC } from 'react';
 
 import { Box, Modal } from '@mui/material';
 
-const SkipRefractModal = () => {
+const SkipRefractModal: VFC = () => {
   const [open, setOpen] = useState(false)
+
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
+
+  const { skipRefract, errorMessage } = useSkipRefract()
 
   return (
     <>
       <Box onClick={handleOpen}>
         <OutlinedRoundedCornerButton onClick={handleOpen} label="スキップ" color="#f9f4ef" />
       </Box>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box />
-        {/* <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box> */}
+      <Modal open={open} onClose={handleClose} aria-labelledby="modal-title">
+        <ModalForRefract
+          title="リフラクトをスキップしますか？"
+          descriptions={[
+            'スキップボタンを押すと、今週のリフラクト機能は使用されません。',
+            'また、この操作はやり直すことができません。',
+          ]}
+        >
+          <Box mb={1}>
+            <ErrorMessage error={errorMessage} />
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            <ContainedRoundedCornerButton onClick={handleClose} label="キャンセル" backgroundColor="#86868b" />
+            <ContainedRoundedCornerButton onClick={skipRefract} label="スキップ" backgroundColor="#2699fb" />
+          </Box>
+        </ModalForRefract>
       </Modal>
     </>
   )
