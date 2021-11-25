f235e171799e6f857cce7c22deb95f331a70a1d7
Ken0806 <ken1min86@gmail.com>
2021-11-25 22:28:35 +0900

    [fix]リフラクトバッチが本番環境で動作するように修正

--- server/config/schedule.rb
+++ server/config/schedule.rb
@@ -3,8 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 ENV.each { |k, v| env(k, v) }
 
 set :output, error: 'log/batch/cron_error.log', standard: 'log/batch/cron.log'
-set :environment, :development
 
 every :friday, at: '8:30 pm' do # UTCで指定している。JSTでは土曜日のAM5:30に作動する。
-  runner 'Batch::Cron::RefractBatch.weekly_set_refract', :environment => :development
+  runner 'Batch::Cron::RefractBatch.weekly_set_refract'
 end
